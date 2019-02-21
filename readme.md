80ad90c23489c30279191460839c556f1ad7eb35
Matt Bernhardt <mjbernha@mit.edu>
2019-02-20 21:18:58 -0500

    Initial commit

---
+++ readme.md
@@ -0,0 +1,15 @@
+# Mais
+
+Mais is a Python project to build a Monte Carlo simulation tool for Major
+League Soccer.
+
+## About the name
+
+[Brian Maisonneuve](https://en.wikipedia.org/wiki/Brian_Maisonneuve) - nicknamed "Mais" - played midfield for the Columbus Crew
+and US National team from 1996 through 2004. He was a member of the 1998 US
+World Cup team, appearing in all three games.
+
+During his playing career, Mais developed a reputation as a player with great
+awareness of the field, and was able to anticipate plays with an uncanny
+regularity. This skillset makes him a suitable inspiration for a tool that
+attempts to predict soccer games.
---
+++ setup.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Mais, a python package for predicting Major League Soccer.
+"""
+
+import io
+from setuptools import find_packages, setup
+
+
+with io.open('LICENSE') as f:
+    license = f.read()
+
+
+setup(
+    name='mais',
+    version='0.0.1',
+    description='Predict games in Major League Soccer',
+    url='https://github.com/matt-bernhardt/mais',
+    license=license,
+    author='Matt Bernhardt',
+    author_email='matt.j.bernhardt@gmail.com',
+    packages=find_packages(exclude=['tests']),
+    install_requires=[
+        'xlrd',
+        'mysql-connector-python-rf',
+    ],
+    dependency_links=[
+        'http://dev.mysql.com/downloads/connector/python/',
+    ],
+    entry_points={
+        'console_scripts': ['mais=mais.command_line:main'],
+    },
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intendend Audience :: Other Audience',
+        'Environment :: Console',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.7',
+    ]
+)
