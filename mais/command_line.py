34f6dace092d54931e4dcecdbd6929e52c72e32f
Matt Bernhardt <mjbernha@mit.edu>
2019-02-21 00:26:49 -0500

    Adds two basic parameters: batch size and operating mode

--- mais/command_line.py
+++ mais/command_line.py
@@ -1,5 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
+import click
 
-def main():
-	print('Hello, world!')
+@click.command()
+@click.option('--batch', '-b', default=1, help='How many seasons should be simulated in a batch?')
+@click.argument('mode', type=click.Choice(['single','each']), default='single')
+
+def main(batch, mode):
+	click.echo('Operating in \'' + mode + '\' mode')
+	click.echo('Batch size: ' + str(batch))
