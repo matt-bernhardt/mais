4739a3355bc3613568d7c59f05bfbfb5eed389f1
Matt Bernhardt <mjbernha@mit.edu>
2019-02-21 21:56:38 -0500

    Adds season parameter

--- mais/command_line.py
+++ mais/command_line.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
+from datetime import date
 import click
 
 @click.command()
@@ -7,10 +8,12 @@ import click
 @click.argument('competition', type=click.Choice(['mls']), default='mls')
 @click.option('--model', '-m', type=click.Choice(['v1','v2']), default='v2', help='Which prediction model should be used?')
 @click.option('--batch', '-b', default=5, help='How many seasons should be simulated in a batch?')
+@click.option('--season', '-s', type=click.IntRange(2011,date.today().year), default=date.today().year, help='What season should be simulated? (2011-present)')
 
-def main(batch, mode, competition, model):
+def main(mode, competition, model, batch, season):
 	# Reflect back the configuration being used
 	click.echo('Competition: ' + competition)
+	click.echo('Season:      ' + str(season))
 	click.echo('Mode:        ' + mode)
 	click.echo('Batch size:  ' + str(batch))
 	click.echo('Using model: ' + model)
