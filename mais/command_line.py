bd28fc859558b6ebaa266095cf3124f0a95e53ac
Matt Bernhardt <mjbernha@mit.edu>
2019-02-21 21:17:45 -0500

    First pass at adding arguments and options via Click

--- mais/command_line.py
+++ mais/command_line.py
@@ -3,9 +3,22 @@ from __future__ import absolute_import
 import click
 
 @click.command()
-@click.option('--batch', '-b', default=1, help='How many seasons should be simulated in a batch?')
 @click.argument('mode', type=click.Choice(['single','each']), default='single')
+@click.argument('competition', type=click.Choice(['mls']), default='mls')
+@click.option('--model', '-m', type=click.Choice(['v1','v2']), default='v2', help='Which prediction model should be used?')
+@click.option('--batch', '-b', default=5, help='How many seasons should be simulated in a batch?')
 
-def main(batch, mode):
-	click.echo('Operating in \'' + mode + '\' mode')
-	click.echo('Batch size: ' + str(batch))
+def main(batch, mode, competition, model):
+	# Reflect back the configuration being used
+	click.echo('Competition: ' + competition)
+	click.echo('Mode:        ' + mode)
+	click.echo('Batch size:  ' + str(batch))
+	click.echo('Using model: ' + model)
+
+	# Initialize
+
+	# Run simulations
+
+	# Output data
+
+	# Visualize?
