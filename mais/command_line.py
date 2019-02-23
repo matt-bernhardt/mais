c729698b003f99ac87a7e462c7bef6af540045e7
Matt Bernhardt <mjbernha@mit.edu>
2019-02-23 18:33:30 -0500

    Fixes indenting

--- mais/command_line.py
+++ mais/command_line.py
@@ -6,15 +6,15 @@ import click
 
 @click.command()
 @click.argument('mode', type=click.Choice(['single', 'each']),
-    default='single')
+        default='single')
 @click.argument('competition', type=click.Choice(['mls']), default='mls')
 @click.option('--model', '-m', type=click.Choice(['v1', 'v2']), default='v2',
-    help='Which prediction model should be used?')
+        help='Which prediction model should be used?')
 @click.option('--batch', '-b', default=5,
-    help='How many seasons should be simulated in a batch?')
+        help='How many seasons should be simulated in a batch?')
 @click.option('--season', '-s', type=click.IntRange(2011, date.today().year),
-    default=date.today().year,
-    help='What season should be simulated? (2011-present)')
+        default=date.today().year,
+        help='What season should be simulated? (2011-present)')
 @click.version_option(message='%(version)s')
 def main(mode, competition, model, batch, season):
     # Reflect back the configuration being used
