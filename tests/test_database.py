0151632d48cfc77285a0ffe6fb61a2a8f4e1e3f2
Matt Bernhardt <mjbernha@mit.edu>
2019-02-23 21:37:05 -0500

    Fixes test inconsistency

--- tests/test_database.py
+++ tests/test_database.py
@@ -13,23 +13,23 @@ def test_database_query():
     d = Database()
     d.connect()
     # Inserts
-    sql = ('INSERT INTO tbl_players '
-           '(FirstName, LastName) '
+    sql = ('INSERT INTO tbl_games '
+           '(MatchTime, MatchTypeID) '
            'VALUES '
            '(%s, %s)')
-    params = ('Delete', 'Me', )
+    params = ('1960-01-01 19:30:00', 1, )
     rs = d.query(sql, params)
     assert d.warnings() is None
     # Updates
-    sql = ('UPDATE tbl_players '
-           'SET Position = %s '
-           'WHERE FirstName = %s AND LastName = %s')
-    rs = d.query(sql, ('Defender', 'Delete', 'Me', ))
+    sql = ('UPDATE tbl_games '
+           'SET HteamID = %s '
+           'WHERE MatchTime = %s AND MatchTypeID = %s')
+    rs = d.query(sql, (1, '1960-01-01 19:30:00', 1, ))
     assert d.warnings() is None
     # Selects
     sql = ('SELECT ID '
-           'FROM tbl_players '
-           'WHERE FirstName = %s AND LastName = %s')
+           'FROM tbl_games '
+           'WHERE MatchTime = %s AND MatchTypeID = %s')
     rs = d.query(sql, params)
     if (rs.with_rows):
         records = rs.fetchall()
@@ -38,8 +38,8 @@ def test_database_query():
     assert needleID > 0
     assert d.warnings() is None
     # Deletes
-    sql = ('DELETE FROM tbl_players '
-           'WHERE FirstName = %s AND LastName = %s')
+    sql = ('DELETE FROM tbl_games '
+           'WHERE MatchTime = %s AND MatchTypeID = %s')
     rs = d.query(sql, params)
     assert d.warnings() is None
     d.disconnect()
@@ -55,15 +55,15 @@ def test_database_lastInsertID():
     d.connect()
     # LAST_INSERT_ID() is zero if you haven't done anything yet
     assert d.lastInsertID() == 0
-    sql = ('INSERT INTO tbl_players '
-           '(FirstName, LastName) '
+    sql = ('INSERT INTO tbl_games '
+           '(MatchTime, MatchTypeID) '
            'VALUES '
            '(%s, %s)')
-    params = ('Delete', 'Me', )
+    params = ('1960-01-01 19:30:00', 1, )
     d.query(sql, params)
     # Now that we've inserted something, LAST_INSERT_ID() should be positive
     assert d.lastInsertID() > 0
     # Clean up our mess
-    sql = ('DELETE FROM tbl_players '
-           'WHERE FirstName = %s AND LastName = %s')
+    sql = ('DELETE FROM tbl_games '
+           'WHERE MatchTime = %s AND MatchTypeID = %s')
     rs = d.query(sql, params)
